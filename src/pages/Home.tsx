5e7c8110d421552d33ccca27750b44726c28dbd7
Davide Coppola <95505688+dvdcpp@users.noreply.github.com>
2024-05-02 11:14:59 +0200

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,32 +2,13 @@ import { useState } from "react";
 import { Form } from "comp-lib/stories/Form";
 import { Footer } from 'comp-lib/stories/Footer'
 import ProductImg from "../assets/product.jpg";
+import ProductImg2 from "../assets/product2.jpg";
 import ProductComponent from "../components/ProductComponent";
-import { handleFormSubmission } from "../../utils/formHandlers";
+import { handleFormSubmission, handlePaymentSubmission } from "../../utils/formHandlers";
 
 const Home = () => {
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [isPaymentSubmitted, setIsPaymentSubmitted] = useState(false);
-
-
-  const handlePaymentSubmission = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const success = true;
-
-    try {
-      const formData = new FormData(event.currentTarget);
-      const data = Object.fromEntries(formData.entries());
-      console.log(data);
-
-      if (success) {
-        setIsPaymentSubmitted(true);
-      } else {
-        console.log("Payment submission failed");
-      }
-    } catch (error) {
-      console.error("Error submitting payment:", error);
-    }
-  };
 
   return (
     <>
@@ -38,14 +19,14 @@ const Home = () => {
             {isFormSubmitted && !isPaymentSubmitted ? (
               <div className="max-w-[500px] w-full mx-auto">
                 <h1 className="text-center text-2xl mb-4"> Payment Information </h1>
-                <form onSubmit={handlePaymentSubmission} className="flex flex-col gap-2">
+                <form onSubmit={(event) => handlePaymentSubmission(event, setIsPaymentSubmitted)} className="flex flex-col gap-2">
                   <div>
                     <label htmlFor="creditNumber">Credit Card Number:</label>
-                    <input type="text" id="creditNumber" name="creditNumber" required className="form-control" placeholder="0000 0000 0000 0000" />
+                    <input type="text" id="creditNumber" name="creditNumber" className="form-control" placeholder="0000 0000 0000 0000" />
                   </div>
                   <div>
                     <label htmlFor="holder">Card Holder Name:</label>
-                    <input type="text" id="holder" name="holder" required className="form-control" placeholder="John Doe" />
+                    <input type="text" id="holder" name="holder" className="form-control" placeholder="John Doe" />
                   </div>
                   <div>
                     <label htmlFor="expire">Expiration Date:</label>
@@ -55,13 +36,12 @@ const Home = () => {
                       name="expire"
                       placeholder="MM/YYYY"
                       pattern="(0[1-9]|1[0-2])\/[0-9]{4}"
-                      required
                       className="form-control"
                     />
                   </div>
                   <div>
                     <label htmlFor="cvv">CVV:</label>
-                    <input type="text" id="cvv" name="cvv" required className="form-control" placeholder="123"/>
+                    <input type="text" id="cvv" name="cvv" className="form-control" placeholder="123"/>
                   </div>
                   <div className="text-center">
                     <input type="submit" className="btn-primary mt-3" value="Submit Payment" />
@@ -76,9 +56,26 @@ const Home = () => {
             ) : (
               <div className="max-w-[500px] w-full mx-auto">
                 <h1 className="text-center mb-4 text-2xl">Fill in your informations</h1>
-                <Form size="short" primary={true} onSubmit={(event) => handleFormSubmission(event, setIsFormSubmitted)} />
+                <Form size="long" primary={true} onSubmit={(event) => handleFormSubmission(event, setIsFormSubmitted)} />
               </div>
             )}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-12 bg-gray-50">
+        <div className="container grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex justify-center items-center">
+            <div className="text-center">
+            <h2 className="text-5xl">
+              Lorem ipsum dolor sit amet.
+            </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, hic.</p>
+            <button className="btn-primary w-fit mt-3">Join us</button>
+            </div>
+          </div>
+          <div>
+            <img src={ProductImg2} alt="" className="rounded-3xl" />
           </div>
         </div>
       </section>
--- utils/formHandlers.ts
+++ utils/formHandlers.ts
@@ -1,18 +1,43 @@
-export const handleFormSubmission = async (event, setIsFormSubmitted) => {
-    event.preventDefault();
-    const success = true;
+export const handleFormSubmission = async (
+  event: React.FormEvent<HTMLFormElement>,
+  setIsFormSubmitted: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  event.preventDefault();
+  const success = true;
 
-    try {
-      const formData = new FormData(event.currentTarget);
-      const data = Object.fromEntries(formData.entries());
-      console.log(data);
+  try {
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
 
-      if (success) {
-        setIsFormSubmitted(true);
-      } else {
-        console.log("Form submission failed");
-      }
-    } catch (error) {
-      console.error("Error submitting form:", error);
+    if (success) {
+      setIsFormSubmitted(true);
+    } else {
+      console.log("Form submission failed");
     }
-  };
+  } catch (error) {
+    console.error("Error submitting form:", error);
+  }
+};
+
+export const handlePaymentSubmission = async (
+  event: React.FormEvent<HTMLFormElement>,
+  setIsPaymentSubmitted: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  event.preventDefault();
+  const success = true;
+
+  try {
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    if (success) {
+      setIsPaymentSubmitted(true);
+    } else {
+      console.log("Payment submission failed");
+    }
+  } catch (error) {
+    console.error("Error submitting payment:", error);
+  }
+};
